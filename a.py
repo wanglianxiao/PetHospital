59dddc65aac988d3480c3cc74d8abf6a5d445a46
wangxiaodui <xiaodui@xiaoduideMacBook-Pro.local>
2017-03-07 14:29:35 +0800

    3.7plus

--- a.py
+++ a.py
@@ -3,14 +3,16 @@ import web
 import json
 from pymongo import *
 
-DB = MongoClient("172.30.235.146", 27017)["aaa"]
+DB = MongoClient("172.30.235.146",21017)["aaa"]
 
 urls = (
     '/test', 'test',
     "/disease", "Disease",
     "/admin", "Admin",
     "/login", "login"
+              "/case", "Case"
 )
+
 
 def success(data):
     return json.dumps(
@@ -22,15 +24,17 @@ def success(data):
         indent=2
     )
 
+
 def fail(message):
     return json.dumps(
         {
-            "status": 200,
+            "status": 400,
             "message": message
         },
         ensure_ascii=False,
         indent=2
     )
+
 
 class BaseClass:
     def __init__(self):
@@ -54,7 +58,7 @@ class BaseClass:
         elif operation == "update":
             return self.update()
 
-    def list(self, key = None):
+    def list(self, key=None):
         List = []
         for item in self.client.find(key):
             data = dict(item)
@@ -62,15 +66,20 @@ class BaseClass:
             List.append(data)
         return success(List)
 
-    def add(self):
-        dict = {}
+    def add(self, default={}):
+        dict = default
         majorValue = web.input().get(self.majorKey)
-        if self.client.find(majorValue):
+        if self.client.find({self.majorKey: majorValue}) is None:
             return fail(majorValue + " already exist")
+        if self.majorKey not in dict:
+            dict[self.majorKey] = majorValue
         for key in self.keys:
+            if key in dict:
+                continue
             value = web.input().get(key)
             dict[key] = value
-        return self.client.insert_one(dict)
+        self.client.insert_one(dict)
+        return success("")
 
     def update(self):
         majorValue = web.input().get(self.majorKey)
@@ -94,81 +103,35 @@ class BaseClass:
         else:
             return success(self.client.delete_one({self.majorKey: value}))
 
+
 class test(BaseClass):
     def __init__(self):
-        self.Class = "test"
+        self.className = "Admin"
+        self.client = DB["user"]
+        self.majorKey = "name"
+        self.keys = ["password", "role"]
 
-class case:
     def GET(self):
-        return "case"
+        return "GET"
 
     def POST(self):
-        client = DB["case"]
-        operation = web.input().get("operation")
-        if operation == "list":
-            list = []
-            for item in client.find():
-                list.append(item["name"])
-            return success(list)
-        elif operation == "add":
-            Class = web.input().get("class")
-            name = web.input().get("name")
-            return success(client["disease"].insert_one({"class": Class, "name": name}))
-        elif operation == "delete":
-            name = web.input().get("name")
-            return success(client["disease"].delete_one({"name": name}))
-        elif operation == "update":
-            name = web.input().get("name")
-            newClass = web.input().get("new_class")
-            newName = web.input().get("new_name")
-            if newClass is None:
-                return client["disease"].update({'name': name}, {"$set": {"name": newName}})
-            elif newName is None:
-                return client["disease"].update({'name': name}, {"$set": {"class": newClass}})
-            else:
-                return client["disease"].update({'name': name}, {"$set": {"name": newName, "class": newClass}})
+        return "POST"
+
+
+class Case:
+    def __init__(self):
+        self.client = DB["case"]
+        self.className = "Case"
+        self.majorKey = ""
+
 
 class Disease(BaseClass):
-    def GET(self):
-        return "disease"
+    def __init__(self):
+        self.client = DB["disease"]
+        self.className = "Disease"
+        self.majorKey = "disease"
+        self.keys = ["type"]
 
-    def POST(self):
-        client = DB["disease"]
-        operation = web.input().get("operation")
-        if operation == "list_disease":
-            type = web.input().get("type")
-            List = []
-            for item in client.find({"type": type}):
-                list.append(item["disease"])
-            return success(List)
-        elif operation == "list_type":
-            S = set()
-            for item in client.find():
-                S.add(item["type"])
-            List = list(S)
-            return success(List)
-        elif operation == "add":
-            type = web.input().get("type")
-            disease = web.input().get("disease")
-            return success(client.insert_one({"type": type, "disease": disease}))
-        elif operation == "delete":
-            disease = web.input().get("disease")
-            return success(client.delete_one({"disease": disease}))
-        elif operation == "update":
-            disease = web.input().get("disease")
-            newtype = web.input().get("new_type")
-            newdisease = web.input().get("new_disease")
-
-            if client.find_one({"disease": disease}) is None:
-                return fail("disease not found")
-            if disease != newdisease and client.find_one({"disease": newdisease}) is not None:
-                return fail("disease already exist")
-            dict = {}
-            if newtype is not None:
-                dict["type"] = newtype
-            if newdisease is not None:
-                dict["disease"] = newdisease
-            return success(client.update({'disease': disease}, {"$set": dict}))
 
 class Admin(BaseClass):
     def __init__(self):
@@ -176,6 +139,10 @@ class Admin(BaseClass):
         self.client = DB["user"]
         self.majorKey = "name"
         self.keys = ["password", "role"]
+
+    def add(self):
+        return BaseClass.add(self, {"role": "internal"})
+
 
 class login:
     def GET(self):
@@ -194,6 +161,7 @@ class login:
             return success(data)
         else:
             return fail("密码错误")
+
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
