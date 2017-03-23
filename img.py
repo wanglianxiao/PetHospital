661d0c7ca2dfb8f49a1d803d94f55e465e09405b
wangxiaodui <xiaodui@xiaoduideMacBook-Pro.local>
2017-03-23 10:48:38 +0800

    more functions

--- img.py
+++ img.py
@@ -1,14 +1,19 @@
-import azure.storage.file
-from azure.storage.file import FileService
-# from azure.storage.file import ContentSettings
+from azure.storage.blob import BlockBlobService
+block_blob_service = BlockBlobService(account_name='wangpiaoliang', account_key='cp9hQ73nKEmVv3RxPynT+Z3AvlvRLjw84GHh+FgkMjgfKv+rG+mHn65ZDLT3BxdhQZnQkoU/KtgWCxs2P1wvmQ==')
 
-file_service = **FileService**(account_name='wangpiaoliang', account_key='cXfnJkmgJgwZH6frhAeqOHSqOrK+KH9Dhau4YGtcIt6ylRDIDnHOu3TQj9lPir7DPecPS1oogU+SNmp1mzyLVg==')
+# block_blob_service.create_container('mycontainer')
+generator = block_blob_service.list_blobs('mei')
+for blob in generator:
+    print(blob.name)
 
-file_service.create_share('mei')
-
-file_service.create_file_from_path(
+from azure.storage.blob import ContentSettings
+block_blob_service.create_blob_from_path(
     'mei',
-    None, # We want to create this blob in the root directory, so we specify None for the directory_name
-    'case',
-    'wanglianxiao.jpeg',
-    content_settings=azure.storage.file.ContentSettings(content_type='image/jpeg'))
+    'WechatIMG81.jpeg',
+    'WechatIMG81.jpeg',
+    content_settings=ContentSettings(content_type='image/jpeg')
+            )
+
+generator = block_blob_service.list_blobs('mei')
+for blob in generator:
+    print(blob.name)
